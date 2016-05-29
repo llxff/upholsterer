6816e2efd7ac115bd57d331c4ed8c6b63c8c1375
llxff <ll.wg.bin@gmail.com>
2016-05-29 20:20:26 +0300

    remove unused code

--- spec/support/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment
-  attr_accessor :body, :created_at, :user
-
-  def initialize(attrs = {})
-    attrs.each {|name, value| __send__("#{name}=", value)}
-  end
-end
--- spec/support/user.rb
+++
@@ -1,7 +0,0 @@
-class User
-  attr_accessor :name, :email, :password_salt, :password_hash
-
-  def initialize(attrs = {})
-    attrs.each {|name, value| __send__("#{name}=", value)}
-  end
-end
