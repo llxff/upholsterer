d69808d10dc6b7bdf9b73309170f66b097c3402d
llxff <ll.wg.bin@gmail.com>
2016-05-29 20:37:14 +0300

    fix to_h

--- spec/support/matchers/json_matcher.rb
+++ spec/support/matchers/json_matcher.rb
@@ -1,9 +1,9 @@
 RSpec::Matchers.define :be_json_with do |expected|
   match do |json_string|
-    actual_hash = JSON.parse(json_string).sort_by(&:first).to_h
-    expected_hash = expected.sort_by(&:first).to_h
+    actual_hash = JSON.parse(json_string).sort_by(&:first)
+    expected_hash = expected.sort_by(&:first)
 
-    actual_hash.to_json == expected_hash.to_json
+    Hash[actual_hash].to_json == Hash[expected_hash].to_json
   end
 end
 
