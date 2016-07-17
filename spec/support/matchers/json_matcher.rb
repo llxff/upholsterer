78928abb9ae06467fff6eb30aeca16209a992406
Александр Фомин <ll.wg.bin@gmail.com>
2016-07-17 10:52:58 +0300

    compare json

--- spec/support/matchers/json_matcher.rb
+++ spec/support/matchers/json_matcher.rb
@@ -1,6 +1,27 @@
 RSpec::Matchers.define :be_json_with do |expected|
   match do |json_string|
-    json_string == expected.to_json
+    current_hash = JSON.parse(json_string)
+
+    compare_hashes(current_hash, expected)
+  end
+
+  def compare_hashes(actual, expected)
+    if actual.keys.count == expected.keys.count
+      actual.all? do |key, value|
+        key = key.to_sym
+        if expected.has_key?(key)
+          expected_value = expected[key]
+
+          if value.is_a? Hash
+            if expected_value.is_a? Hash
+              compare_hashes(value, expected_value)
+            end
+          else
+            expected_value == value
+          end
+        end
+      end
+    end
   end
 end
 
